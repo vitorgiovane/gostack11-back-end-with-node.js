16dc9911b3916a79f325f034d7a2974f80bdbb39
Vitor Giovane <vitorgpalves@gmail.com>
2020-04-10 21:54:08 -0300

    feat: adiciona métodos http

--- src/index.js
+++ src/index.js
@@ -2,8 +2,44 @@ const express = require('express')
 
 const server = express()
 
-server.get('/', (request, response) => response.json({ message: "Hello, Vitor!" }))
+server.get('/projects', (request, response) => {
+  response.json([
+    { id: 1, name: "Project 1" },
+    { id: 2, name: "Project 2" },
+    { id: 3, name: "Project 3" }
+  ])
+})
+
+server.get('/projects/:id', (request, response) => {
+  const id = parseInt(request.params.id) || 1
+  response.json([
+    { id, name: `Project ${request.params.id}` }
+  ])
+})
+
+server.post('/projects', (request, response) => {
+  response.json([
+    { id: 1, name: "Project 1" },
+    { id: 2, name: "Project 2" },
+    { id: 3, name: "Project 3" },
+    { id: 4, name: "Project 4" }
+  ])
+})
+
+server.put('/projects/:id', (request, response) => {
+  const id = parseInt(request.params.id) || 1
+  response.json([
+    { id, name: `Project ${request.params.id}` }
+  ])
+})
+
+server.delete('/projects/:id', (request, response) => {
+  response.json([
+    { id: 2, name: "Project 2" },
+    { id: 3, name: "Project 3" }
+  ])
+})
 
 server.listen(3333, () => {
-  console.log("...\n🚀️ back-end started\n")
+  console.log("...\n🚀️ The back-end has started!\n")
 })
