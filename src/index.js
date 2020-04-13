539a3484db575cbc476c89bee2051083f7562576
Vitor Giovane <vitorgpalves@gmail.com>
2020-04-13 19:01:49 -0300

    feat: remove ponto e vírgula do escopo da função isEmpty

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const { uuid, isUuid } = require('uuidv4')
 server.use(express.json())
 
 const isEmpty = obj => {
-  if (obj == null) return true;
-  if (Array.isArray(obj) || typeof (obj) === 'string') return obj.length === 0;
+  if (obj == null) return true
+  if (Array.isArray(obj) || typeof (obj) === 'string') return obj.length === 0
   if (typeof (obj) === 'object') return Object.keys(obj).length === 0 && obj.constructor === Object
-  return false;
-};
+  return false
+}
 
 const logRequests = (request, response, next) => {
   const { method, url } = request
